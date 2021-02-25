ba02618cbdbfb60096f4ef5aa0faead8913eb59f
Gurutz Larrea <you@example.com>
2021-02-25 16:36:18 +0100

    circle box effects.2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 anime({
+  //CSS selector
   targets: '#tunnel circle',
+  //we scale it up by 20% 
   scale: 1.2,
-  direction: 'alternate',
+  //loops over and over
   loop: true,
-  
+  //from begining to end
+  direction: 'alternate',
+  duration: 250,
+  easing: 'easeInOutCubic',
+  //takes element by element, and delays each of them by 50
+  delay: (el, index) => index * 50,
 });
